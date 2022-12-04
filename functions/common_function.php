8192101345c71651797ad22715154971ef085100
Xolani Mokhoebane <70220609+xolani-mokJS@users.noreply.github.com>
2022-12-04 18:42:34 +0200

    functions

---
+++ functions/common_function.php
@@ -0,0 +1,5 @@
+<?php
+// add to cart 
+
+
+?>
--- includes/connect.php
+++ includes/connect.php
@@ -3,7 +3,6 @@ $con = mysqli_connect('localhost', 'root', '', 'product_database');
 
     if($con->connect_error){
         die("Connection failed: " . $con->connect_error);
-        echo 'Connecting unsuccessful';
     } else  {  
     echo 'Connecting successful';
     }
